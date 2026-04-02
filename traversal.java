446bd54a8588e49f719fb9489459ad7eb18666ad
https://github.com/VaishnaviNishidkar <vnrajxeog@gmail.com>
2026-04-02 14:34:09 +0530

    Added array traversal operation

---
+++ traversal.java
@@ -0,0 +1,13 @@
+public class traversal {
+
+    public static void traverse(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 5, 2};
+        traverse(arr);
+    }
+}
